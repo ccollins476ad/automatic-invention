60ed30fda2613f4ee16e97baa9c04166dd1511ce
David Lee <34385265+dlee-rt@users.noreply.github.com>
2018-11-07 14:39:01 -0800

    update script to combine jenkins jobs
    Previously, two separate jenkins jobs called this script to alternate between two nightly builds. This changes that behavior so that one jenkins job can query the device state, and start an upgrade based on which one is currently running on the device.

--- fwup.sh
+++ fwup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash -e
 
-if [[ $# -lt 3 ]]; then
-    echo "Usage: $0 <device group> <hardware ID> <file_containing_build_id>"
+if [[ $# -lt 4 ]]; then
+    echo "Usage: $0 <device group> <hardware ID> <build ID file 1> <build ID file 2>"
     exit 1
 fi
 
 dg=$1
 hwid=$2
-build_id=$(cat $3)
+build_a=$(cat $3)
+build_b=$(cat $4)
 
 echo $RUNTIME_CI_LINK
 
-runtime job fwup -g $dg -d $hwid -i $build_id -w --timeout=20m
+current=$(runtime device state -g rt-test -d f5f3e2eb64502638a4224861972c717a | jq -r .build_id)
+
+if [[ $current = $build_a ]]; then
+    runtime job fwup -g $dg -d $hwid -i $build_b -w --timeout=20m
+else
+    runtime job fwup -g $dg -d $hwid -i $build_a -w --timeout=20m
+fi
