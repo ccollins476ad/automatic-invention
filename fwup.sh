7578fe759695c2b3cdbe440449a505b8784b3eb9
David Lee <34385265+dlee-rt@users.noreply.github.com>
2018-11-09 13:03:40 -0800

    add a third build to rotate an image off device
    previously, the script would perform dfu between two devices. however, the gateway would just swap the images in slot 0 and slot 1. this adds a third image into the rotation to make sure a full upload and fwu is performed.

--- fwup.sh
+++ fwup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-if [[ $# -lt 4 ]]; then
-    echo "Usage: $0 <device group> <hardware ID> <build ID file 1> <build ID file 2>"
+if [[ $# -lt 5 ]]; then
+    echo "Usage: $0 <device group> <hardware ID> <build ID file 1> <build ID file 2> <build ID file 3>"
     exit 1
 fi
 
@@ -9,6 +9,7 @@ dg=$1
 hwid=$2
 build_a=$(cat $3)
 build_b=$(cat $4)
+build_c=$(cat $5)
 
 echo $RUNTIME_CI_LINK
 
@@ -16,6 +17,8 @@ current=$(runtime device state -g $dg -d $hwid | jq -r .build_id)
 
 if [[ $current = $build_a ]]; then
     runtime job fwup -g $dg -d $hwid -i $build_b -w --timeout=20m
-else
+elif [[ $current = build_b ]]; then
+    runtime job fwup -g $dg -d $hwid -i $build_c -w --timeout=20m
+else 
     runtime job fwup -g $dg -d $hwid -i $build_a -w --timeout=20m
 fi
