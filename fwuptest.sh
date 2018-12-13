dc6db915262990bca889f5c5ee42b351000ce98f
David Lee <34385265+dlee-rt@users.noreply.github.com>
2018-12-12 17:36:42 -0800

    Update and rename fwupnightly.sh to fwuptest.sh

---
+++ fwuptest.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+if [[ $# -lt 5 ]]; then
+    echo "Usage: $0 <device group> <hardware ID> <build ID file 1> <build ID file 2> <build ID file 3>"
+    exit 1
+fi
+
+dg=$1
+hwid=$2
+build_a=$(cat $3)
+build_b=$(cat $4)
+build_c=$(cat $5)
+
+echo $RUNTIME_CI_LINK
+
+current=$(runtime device state -g $dg -d $hwid | jq -r .build_id)
+
+function run_upgrade {
+    if fwup_out=$(runtime job fwup -g $1 -d $2 -i $3 -w --timeout=20min); then
+        echo "Success: $fwup_out"
+    else 
+        err=${fwup_out#"error: "}
+        echo "error from gateway: $err"
+        exit 1
+    fi
+}
+
+if [[ $current = $build_a ]]; then
+    run_upgrade $dg $hwid $build_b
+elif [[ $current = $build_b ]]; then
+    run_upgrade $dg $hwid $build_c
+else 
+    run_upgrade $dg $hwid $build_b
+fi
